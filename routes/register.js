31f2eb8eb7178529261d86bc6c1f0be96f6eb681
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-22 15:28:03 +0100

    update

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,13 @@
 /*@AlexWirthAAU
     Routen zur Abhandlung der registration.  
 */
+const express = require('express');
+const router = express.Router();
 
-app.post('/reg', (req, res) => {
+
+router.post('/', (req, res) => {
     console.log(req);
     res.status(200).json({message: "user registered"});
 })
+
+module.exports = router;
