743f51cfbd0a5b8a66c049b28d89eea3ee494369
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-22 15:10:32 +0100

    register post

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,8 @@
 /*@AlexWirthAAU
     Routen zur Abhandlung der registration.  
 */
+
+app.post('/', (req, res) => {
+    console.log(req);
+    res.status(200).json({message: "user registered"});
+})
