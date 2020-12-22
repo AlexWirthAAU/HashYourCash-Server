eef80f219ed2deb6ccde0de8bb8d2a6a33a1cabe
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-22 15:25:55 +0100

    test

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@
     Routen zur Abhandlung der registration.  
 */
 
-app.post('/', (req, res) => {
+app.post('/reg', (req, res) => {
     console.log(req);
     res.status(200).json({message: "user registered"});
 })
