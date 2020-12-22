fbb55830d763c816a3a4325ad661092c276bcf8c
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-22 15:19:28 +0100

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true})); //support encoded bodies
 app.use(bodyParser.json()); //support json bodies
 
-const loginRoutes = require('.routes/login');
+
 const registerRoutes = require('.routes/register');
 
 app.use("/register", registerRoutes)
