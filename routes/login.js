6ac915f68be638ad95fca93ebe551c64130b1cab
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-23 07:41:59 +0100

    login routing

--- routes/login.js
+++ routes/login.js
@@ -4,3 +4,25 @@
 const express = require('express');
 const router = express.Router();
 const getDb = require("../database").getDb;
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+router.post('/', (req, res) => {
+    const db = getDb();
+    let email = req.body.email;
+    let password = req.body.password;
+    
+
+    const statement = "SELECT u_password FROM users WHERE email = $1"
+    const values = [email];
+
+    db.query(statement, values, (err, result) => {
+        if (err) {
+            console.error("DB error: ", err.message)
+            res.status(500).json({ message: "an error occured" });
+        } else {
+            console.log(result)
+            res.status(200).json({ message: "user logedin" });
+        }
+    })
+})
