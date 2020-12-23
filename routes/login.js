d7867b28f21b2a53de4bf46efc6df1b7d4673546
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-23 08:11:47 +0100

    update login token creation

--- routes/login.js
+++ routes/login.js
@@ -5,8 +5,8 @@ const express = require('express');
 const router = express.Router();
 const getDb = require("../database").getDb;
 const bcryptjs = require('bcryptjs');
-const saltRounds = 10;
-
+const jwt = require('jsonwebtoken');
+const cfg = require('../config.json')
 
 router.post('/', (req, res) => {
     const db = getDb();
@@ -14,16 +14,44 @@ router.post('/', (req, res) => {
     let password = req.body.password;
     
 
-    const statement = "SELECT u_password FROM users WHERE email = $1"
+    const statement = "SELECT * FROM users WHERE email = $1"
     const values = [email];
 
     db.query(statement, values, (err, result) => {
         if (err) {
             console.error("DB error: ", err.message)
-            res.status(500).json({ message: "an error occured" });
+            res.status(500).json({ message: "an error occured when logging in" });
         } else {
-            console.log(result)
-            res.status(200).json({ message: "user logedin" });
+            if(result.rows.length > 1) {
+                console.error("DB error (more than two pw): ", err.message)
+                res.status(500).json({ message: "an error occured when logging in" });
+            } else {
+                bcryptjs.compare(password, result.rows[0].u_password, function(err, result) {
+                    if(err) {
+                        console.error("Hasihing error: ", err.message)
+                        resp.status(500).json({message: "an error occured with hasing password"});
+                    }
+                    if(result) {
+                        let token = jwt.sign({
+                            u_id: result.rows[0].u_id
+                        }, cfg.database.jwt_secret, {
+                            expiresIn: '24h'
+                        });
+
+                        const text = "UPDATE users SET token = $1 WHERE email = $2";
+                        const vals = [token, email]
+
+                        db.query(text, vals, function(err, result){
+                            if(err) {
+                                console.error("DB-Error in updating: ", err.message)
+                                resp.status(500).json({message: "an error occured"});
+                            } else {
+                                console.log("Updating with token was successful")
+                            }
+                        })
+                    }
+                })
+            }
         }
     })
 })
