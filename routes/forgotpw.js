a907ffe1416db781ac6bdd3a78164c419cb9a55a
AlexWirthAAU <alexwi@edu.aau.at>
2020-12-26 16:25:59 +0100

    logging

--- routes/forgotpw.js
+++ routes/forgotpw.js
@@ -38,6 +38,7 @@ router.post('/reset', (req, res) => {
             res.status(200).json({ message: result })
         })
         .catch(err => {
+            console.log(err)
             res.status(500).json({ message: err })
         })
 
