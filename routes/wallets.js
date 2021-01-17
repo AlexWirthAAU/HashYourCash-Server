3ca12f368ced034cf8e1a600a4b3e16427b10991
Jana Siebert <jsiebert@edu.aau.at>
2021-01-18 00:33:05 +0100

    wallet amount calculations

--- routes/wallets.js
+++ routes/wallets.js
@@ -1,6 +1,6 @@
 //@Zoë Wallets hinzufügen
 
-
+const payments = require('./payments');
 const express = require('express');
 const router = express.Router();
 const getDb = require("../database").getDb;
@@ -14,11 +14,20 @@ router.get("/", checkAuth, (req, res) => {
             res.status(200).json(data);
         })
         .catch(err => {
-            res.status(500).json({ message: "an error occured: " + err.message });
+            res.status(500).json({message: "an error occured: " + err.message});
         })
-})
+});
 
-
+router.get('/:w_id/amounts', checkAuth, (req, res) => {
+    let walletId = req.params.w_id;
+    payments.calcWalletAmounts(walletId)
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(500).json({message: "an error occured: " + err.message});
+        })
+});
 
 router.post('/', checkAuth, (req, res) => {
     let wallets = req.body;
@@ -26,24 +35,24 @@ router.post('/', checkAuth, (req, res) => {
 
     createW(wallets, userId)
         .then(result => {
-            res.status(200).json({ message: result });
+            res.status(200).json({message: result});
         })
         .catch(err => {
-            res.status(500).json({ message: err });
+            res.status(500).json({message: err});
         })
-})
+});
 
 router.delete("/:wallet", checkAuth, (req, res) => {
     let walletId = req.params.wallet;
 
     deleteW(walletId)
         .then((result) => {
-            res.status(200).json({ message: result });
+            res.status(200).json({message: result});
         })
         .catch(err => {
-            res.status(500).json({ message: "an error occured: " + err.message });
+            res.status(500).json({message: "an error occured: " + err.message});
         })
-})
+});
 
 router.put("/:wallet", checkAuth, (req, res) => {
     let walletId = req.params.wallet;
@@ -51,31 +60,31 @@ router.put("/:wallet", checkAuth, (req, res) => {
 
     editW(walletId, wallets)
         .then((result) => {
-            res.status(200).json({ message: result });
+            res.status(200).json({message: result});
         })
         .catch(err => {
-            res.status(500).json({ message: "an error occured: " + err.message });
+            res.status(500).json({message: "an error occured: " + err.message});
         })
-})
+});
 
-function showW(u_id){
+function showW(u_id) {
 
-        const statement = "SELECT w_id, name, description, amount FROM wallet WHERE u_id = $1";
-        const values = [u_id]
-    
-        return new Promise((resolve, reject) => {
-            db.query(statement, values, (err, result) => {
-                if (err) {
-                    console.error("DB error when getting wallets: ", err.message);
-                    reject("DB ERROR: ", err.message);
-                } else {
-                    resolve(result.rows)
-                }
-            })
+    const statement = "SELECT w_id, name, description, amount FROM wallet WHERE u_id = $1";
+    const values = [u_id];
+
+    return new Promise((resolve, reject) => {
+        db.query(statement, values, (err, result) => {
+            if (err) {
+                console.error("DB error when getting wallets: ", err.message);
+                reject("DB ERROR: ", err.message);
+            } else {
+                resolve(result.rows)
+            }
         })
+    })
 }
 
-function createW(walletsData, u_id){
+function createW(walletsData, u_id) {
     return new Promise((resolve, reject) => {
         const statement = "INSERT INTO wallet (u_id, name, description, amount) VALUES ($1, $2, $3, $4)";
         const values = [u_id, walletsData.name, walletsData.description, walletsData.amount];
@@ -86,11 +95,11 @@ function createW(walletsData, u_id){
             } else {
                 resolve("Wallet erstellt")
             }
-    }
-        )})
+        })
+    })
 }
 
-function deleteW(walletId){
+function deleteW(walletId) {
     return new Promise((resolve, reject) => {
         const statement = "DELETE FROM wallet WHERE w_id = $1";
         const values = [walletId];
@@ -101,12 +110,12 @@ function deleteW(walletId){
             } else {
                 resolve("Wallet gelöscht")
             }
-    }
-        )})
+        })
+    })
 
 }
 
-function editW(walletId, walletData){
+function editW(walletId, walletData) {
     return new Promise((resolve, reject) => {
         const statement = "UPDATE wallet SET name = $1, description = $2, amount = $3 WHERE w_id = $4";
         const values = [walletData.name, walletData.description, walletData.amount, walletId];
@@ -117,8 +126,8 @@ function editW(walletId, walletData){
             } else {
                 resolve("Wallet geändert")
             }
-    }
-        )})
+        })
+    })
 
 }
 
